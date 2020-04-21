7fbfddae813a3dba345261b012e809a58be71e4c
Mantas Kūjalis <mantas.kujalis@vinted.com>
2020-04-21 15:09:46 +0300

    Update class nesting

--- lib/chained_job/start_chains.rb
+++ lib/chained_job/start_chains.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
-class ChainedJob::StartChains
-  def self.run(target_class, array_of_job_arguments, parallelism)
-    new(target_class, array_of_job_arguments, parallelism).run
-  end
+module ChainedJob
+  class StartChains
+    def self.run(target_class, array_of_job_arguments, parallelism)
+      new(target_class, array_of_job_arguments, parallelism).run
+    end
 
-  attr_reader :target_class, :array_of_job_arguments, :parallelism
+    attr_reader :target_class, :array_of_job_arguments, :parallelism
 
-  def initialize(target_class, array_of_job_arguments, parallelism)
-    @target_class = target_class
-    @array_of_job_arguments = array_of_job_arguments
-    @parallelism = parallelism
-  end
+    def initialize(target_class, array_of_job_arguments, parallelism)
+      @target_class = target_class
+      @array_of_job_arguments = array_of_job_arguments
+      @parallelism = parallelism
+    end
 
-  def run
-    # store array_of_job_arguments to redis
-    # start jobs:
-    # parallelism.times { |worked_id| target_class.perform_later(worked_id) }
+    def run
+      # store array_of_job_arguments to redis
+      # start jobs:
+      # parallelism.times { |worked_id| target_class.perform_later(worked_id) }
+    end
   end
 end
