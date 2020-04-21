5e1bb6638447478f6fd55abe931cbfa3371540f8
Mantas Kūjalis <mantas.kujalis@vinted.com>
2020-04-21 14:16:07 +0300

    Simplify middleware

--- lib/chained_job/middleware.rb
+++ lib/chained_job/middleware.rb
@@ -5,19 +5,11 @@ require 'chained_job/process'
 
 module ChainedJob
   module Middleware
-    def self.included(target_class)
-      target_class.include ClassMethods
-    end
-
-    module ClassMethods
-      module_function
-
-      def perform(worked_id = nil)
-        if worked_id
-          ChainedJob::Process.run(self, worked_id)
-        else
-          ChainedJob::StartChains.run(self.class, array_of_job_arguments, parallelism)
-        end
+    def perform(worked_id = nil)
+      if worked_id
+        ChainedJob::Process.run(self, worked_id)
+      else
+        ChainedJob::StartChains.run(self.class, array_of_job_arguments, parallelism)
       end
     end
   end
