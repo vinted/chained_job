1a8cd7897abab28880bd51e843c55a1f22279f0a
Mantas Kūjalis <45848524+mantaskujalis@users.noreply.github.com>
2020-04-22 12:13:38 +0300

    Add a logger (#6)

--- lib/chained_job/config.rb
+++ lib/chained_job/config.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'logger'
+
 module ChainedJob
   class Config
     DEFAULT_ARGUMENTS_BATCH_SIZE = 1_000
@@ -7,11 +9,14 @@ module ChainedJob
     attr_accessor(
       :arguments_batch_size,
       :debug,
+      :logger,
       :redis,
     )
 
     def initialize
       self.arguments_batch_size = DEFAULT_ARGUMENTS_BATCH_SIZE
+
+      self.logger = ::Logger.new(STDOUT)
 
       self.debug = true
     end
