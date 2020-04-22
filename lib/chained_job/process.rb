aa798653a47abb1948b72857f693b713bbafc3aa
Mantas Kūjalis <45848524+mantaskujalis@users.noreply.github.com>
2020-04-22 12:35:44 +0300

    Implement job processing (#5)
    * Implement job processing
    
    * Adjust naming

--- lib/chained_job/process.rb
+++ lib/chained_job/process.rb
@@ -2,24 +2,32 @@
 
 module ChainedJob
   class Process
-    def self.run(target, worked_id)
-      new(target, worked_id).run
+    def self.run(job_instance, worker_id)
+      new(job_instance, worker_id).run
     end
 
-    attr_reader :target, :worked_id
+    attr_reader :job_instance, :worker_id
 
-    def initialize(target, worked_id)
-      @target = target
-      @worked_id = worked_id
+    def initialize(job_instance, worker_id)
+      @job_instance = job_instance
+      @worker_id = worker_id
     end
 
     def run
-      # get arg from redis
-      # return if arg is nil
-      # else start process:
-      # target.process(2)
-      # start another job:
-      # target.class.perform_later(worked_id)
+      return unless argument
+
+      job_instance.process(argument)
+      job_instance.class.perform_later(worker_id)
+    end
+
+    private
+
+    def argument
+      @argument ||= ChainedJob.redis.lpop(redis_key)
+    end
+
+    def redis_key
+      "chained_job:#{job_instance.class}"
     end
   end
 end
---
+++ test/chained_job/process_test.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require 'mock_redis'
+require 'minitest/autorun'
+
+class ChainedJob::ProcessTest < Minitest::Test
+  def test_process_chain
+    job_instance.expect(:class, job_class, [])
+    job_instance.expect(:process, nil, ['1'])
+    job_instance.expect(:class, job_class, [])
+    job_class.expect(:perform_later, nil, [1])
+
+    tested_class.run(job_instance, 1)
+
+    job_instance.verify
+  end
+
+  def test_empty_arguments_queue
+    redis.lpop(redis_key)
+
+    job_instance.expect(:class, job_class, [])
+    tested_class.run(job_instance, 1)
+
+    job_instance.verify
+  end
+
+  private
+
+  def setup
+    ChainedJob.configure do |config|
+      config.redis = redis
+    end
+
+    redis.rpush(redis_key, %w(1))
+  end
+
+  def teardown
+    ChainedJob.instance_variable_set(:@config, nil)
+  end
+
+  def job_instance
+    @job_instance ||= MiniTest::Mock.new
+  end
+
+  def redis
+    @redis ||= MockRedis.new
+  end
+
+  def redis_key
+    "chained_job:#{job_class}"
+  end
+
+  def job_class
+    @job_class ||= MiniTest::Mock.new
+  end
+
+  def tested_class
+    ChainedJob::Process
+  end
+end
