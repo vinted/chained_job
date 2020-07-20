c6c752a6ab2a9b3ba40aedc93edc1909c24f422d
Mantas Kūjalis <mantas.kujalis@vinted.com>
2020-07-20 15:50:41 +0300

    Use variable for redis key

--- lib/chained_job/clean_up_queue.rb
+++ lib/chained_job/clean_up_queue.rb
@@ -22,9 +22,10 @@ module ChainedJob
 
         break unless tag
 
-        size = redis.llen(redis_key(tag))
-        (size / TRIM_STEP_SIZE).times { redis.ltrim(redis_key(tag), 0, -TRIM_STEP_SIZE) }
-        redis.del(redis_key(tag))
+        redis_key = Helpers.redis_key(job_key, tag)
+        size = redis.llen(redis_key)
+        (size / TRIM_STEP_SIZE).times { redis.ltrim(redis_key, 0, -TRIM_STEP_SIZE) }
+        redis.del(redis_key)
       end
     end
 
@@ -32,10 +33,6 @@ module ChainedJob
 
     def tag_list
       @tag_list ||= Helpers.tag_list(job_key)
-    end
-
-    def redis_key(tag)
-      @redis_key ||= Helpers.redis_key(job_key, tag)
     end
 
     def job_key
