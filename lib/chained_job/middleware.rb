6651e95152932ed6944f3e90df7b236582d4a8fb
Mantas Kūjalis <mantas.kujalis@vinted.com>
2020-10-19 09:45:26 +0300

    Add around_array_of_job_arguments hook

--- lib/chained_job/middleware.rb
+++ lib/chained_job/middleware.rb
@@ -13,8 +13,13 @@ module ChainedJob
       if worker_id
         ChainedJob::Process.run(self, worker_id, tag)
       else
-        ChainedJob::StartChains.run(self.class, array_of_job_arguments, parallelism)
+        ChainedJob::StartChains.run(self.class, arguments_array, parallelism)
       end
+    end
+
+    def arguments_array
+      options = { job_class: self.class }
+      ChainedJob.config.around_array_of_job_arguments.call(options) { array_of_job_arguments }
     end
 
     def array_of_job_arguments
@@ -26,3 +31,4 @@ module ChainedJob
     end
   end
 end
+
